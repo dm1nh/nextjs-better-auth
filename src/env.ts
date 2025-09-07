3537d66172802b21d0e0f03243fcd132959a7280
dm1nh <dangminhngo.dev@gmail.com>
2025-09-07 19:06:19 +0700

    chore: add t3-env

---
+++ src/env.ts
@@ -0,0 +1,12 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    DATABASE_URL: z.url(),
+  },
+  client: {},
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  experimental__runtimeEnv: {},
+})
